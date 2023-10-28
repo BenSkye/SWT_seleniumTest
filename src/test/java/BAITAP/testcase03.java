655989767449acbc3ff434e4791965cc547f6165
BenSkye <nhatdm9a7@gmail.com>
2023-10-28 07:47:22 +0700

    Test: 01-02

---
+++ src/test/java/BAITAP/testcase03.java
@@ -0,0 +1,30 @@
+package BAITAP;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+/*
+---------------------------------------TC03---------------------------------------
+
+Test Steps:
+
+1. Go to http://live.techpanda.org/
+
+2. Click on �MOBILE� menu
+
+3. In the list of all mobile , click on �ADD TO CART� for Sony Xperia mobile
+
+4. Change �QTY� value to 1000 and click �UPDATE� button. Expected that an error is displayed
+
+"The requested quantity for "Sony Xperia" is not available.
+
+5. Verify the error message
+
+6. Then click on �EMPTY CART� link in the footer of list of all mobiles. A message "SHOPPING CART IS EMPTY" is shown.
+
+7. Verify cart is empty */
+public class testcase03 {
+
+}
